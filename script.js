ce82e2c90e74b7d0c8b5932ef1bbee20e864e6f6
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2025-05-10 19:29:54 +0600

    quote generator complete

---
+++ script.js
@@ -0,0 +1,65 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+//show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+function newQuote() {
+  loading();
+  // pick a random quote
+  const quote =
+    apiQuotes.quotes[Math.floor(Math.random() * apiQuotes.quotes.length)];
+  console.log(quote);
+
+  authorText.textContent = quote.author ? quote.author : "Unknown";
+  quoteText.textContent = quote.quote;
+
+  if (quote.quote.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://dummyjson.com/quotes?limit=0";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuotes();
